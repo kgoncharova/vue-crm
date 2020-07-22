2912c9e4c3bc05649d5daa50e2cea85658ce1aa2
Kristina <codingkris@yahoo.com>
2020-07-22 11:14:29 +0300

    Add firebase

--- src/main.js
+++ src/main.js
@@ -8,14 +8,35 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE,
+  authDomain: "vuecrm-7c0f1.firebaseapp.com",
+  databaseURL: "https://vuecrm-7c0f1.firebaseio.com",
+  projectId: "vuecrm-7c0f1",
+  storageBucket: "vuecrm-7c0f1.appspot.com",
+  messagingSenderId: "656709666677",
+  appId: "1:656709666677:web:6c5bf4150c3f47a45a353c",
+  measurementId: "G-BRB2WRM2MN"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
