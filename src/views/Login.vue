3c7dce5b0260082f17385d6b4fe63824c2308619
Kristina <codingkris@yahoo.com>
2020-07-22 11:35:48 +0300

    Authorization with firebase

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -76,7 +76,7 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -85,7 +85,11 @@ export default {
         email: this.email,
         password: this.password
       }
-      this.$router.push('/')
+
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
