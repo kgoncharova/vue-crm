edf823d498acda608c9762924ddf8fec56928ac5
Kristina <codingkris@yahoo.com>
2020-08-12 13:24:48 +0300

    Fix routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,6 +24,7 @@
 <script>
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
+import messages from '@/utils/messages'
 
 export default {
   name: 'main-layout',
@@ -40,6 +41,17 @@ export default {
   },
   components: {
     Navbar, Sidebar
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      console.log(fbError)
+      this.$error((messages[fbError.code]) || 'Что-то пошло не так')
+    }
   }
 }
 </script>
