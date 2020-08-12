c50ed3e6fb7bc68d07086f559f9dc4f319cd6e63
Kristina <codingkris@yahoo.com>
2020-08-12 14:39:14 +0300

    Add tooltips

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -15,7 +15,7 @@
           <strong>{{cat.title}}</strong>
           {{cat.spend | currency}} из {{cat.limit | currency}}
         </p>
-        <div class="progress" >
+        <div class="progress" v-tooltip="cat.tooltip">
           <div
               class="determinate"
               :class="[cat.progressColor]"
@@ -29,6 +29,7 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import currencyFilter from '@/filters/currency.filter'
 
 export default {
   name: 'planning',
@@ -59,11 +60,15 @@ export default {
          ? 'yellow'
          : 'red'
       
+      const tooltipValue = cat.limit - spend
+      const tooltip = `${tooltip < 0 ? 'Превышение на' : 'Осталось'} ${currencyFilter(Math.abs(tooltipValue))}`
+      
       return {
         ...cat,
         progressPercent,
         progressColor,
-        spend
+        spend,
+        tooltip
       }
     })
 
